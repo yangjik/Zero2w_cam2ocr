82fa9a7e66094f08d039d9ffefdfd1a73db1b215
yangjik <dbswlrl@naver.com>
2025-12-26 18:24:32 +0900

    251226_1
    제로2w에서 사용할 패키지리스트 및 바로실행할 sh파일

---
+++ auto_setting_sh/00_python_package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 준간에 실패하면 중단
+set -e -o pipefail
+
+# 패키지 whl파일 다운 <- zero2w는 cpu, ram 이 작기때문에 가상환경 사용 x
+base_path=~/zero2w_cam2ocr
+err_log=~/zero2w_cam2ocr/download_whl
+
+cd $base_path
+
+pip download -r $base_path/requirements.txt -d $base_path/download_whl --only-binary=:all: -v | tee $err_log/download_err.log
+
+pip install --no-index --find-links="$base_path/download_whl" -r $base_path/requirements.txt -v | tee $err_log/install_err.log
